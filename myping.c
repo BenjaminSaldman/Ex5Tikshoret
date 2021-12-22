f668d457861b096f603ec54012afab7727c92a75
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2021-12-22 12:35:35 +0200

    Update myping.c

--- myping.c
+++ myping.c
@@ -131,6 +131,82 @@ int main ()
     char data[IP_MAXPACKET] = "This is the ping.\n";
 
     int datalen = strlen(data) + 1;
+
+    //==================
+    // IP header
+    //==================
+
+    // IP protocol version (4 bits)
+    iphdr.ip_v = 4;
+
+    // IP header length (4 bits): Number of 32-bit words in header = 5
+    iphdr.ip_hl = IP4_HDRLEN / 4; // not the most correct
+
+    // Type of service (8 bits) - not using, zero it.
+    iphdr.ip_tos = 0;
+
+    // Total length of datagram (16 bits): IP header + ICMP header + ICMP data
+    iphdr.ip_len = htons (IP4_HDRLEN + ICMP_HDRLEN + datalen);
+
+    // ID sequence number (16 bits): not in use since we do not allow fragmentation
+    iphdr.ip_id = 0;
+
+    // Fragmentation bits - we are sending short packets below MTU-size and without 
+    // fragmentation
+    int ip_flags[4];
+
+    // Reserved bit
+    ip_flags[0] = 0;
+
+    // "Do not fragment" bit
+    ip_flags[1] = 0;
+
+    // "More fragments" bit
+    ip_flags[2] = 0;
+
+    // Fragmentation offset (13 bits)
+    ip_flags[3] = 0;
+
+    iphdr.ip_off = htons ((ip_flags[0] << 15) + (ip_flags[1] << 14)
+                      + (ip_flags[2] << 13) +  ip_flags[3]);
+
+    // TTL (8 bits): 128 - you can play with it: set to some reasonable number
+    iphdr.ip_ttl = 128;
+
+    // Upper protocol (8 bits): ICMP is protocol number 1
+    iphdr.ip_p = IPPROTO_ICMP;
+
+    // Source IP
+    if (inet_pton (AF_INET, SOURCE_IP, &(iphdr.ip_src)) <= 0) 
+    {
+        fprintf (stderr, "inet_pton() failed for source-ip with error: %d"
+#if defined _WIN32
+			, WSAGetLastError()
+#else
+			, errno
+#endif
+			);
+        return -1;
+    }
+
+    // Destination IPv
+    if (inet_pton (AF_INET, DESTINATION_IP, &(iphdr.ip_dst)) <= 0)
+    {
+        fprintf (stderr, "inet_pton() failed for destination-ip with error: %d" 
+#if defined _WIN32
+			, WSAGetLastError()
+#else
+			, errno
+#endif
+			);
+        return -1;
+    }
+
+    // IPv4 header checksum (16 bits): set to 0 prior to calculating in order not to include itself.
+    iphdr.ip_sum = 0;
+    iphdr.ip_sum = calculate_checksum((unsigned short *) &iphdr, IP4_HDRLEN);
+
+
     //===================
     // ICMP header
     //===================
@@ -194,7 +270,7 @@ int main ()
 
     // Create raw socket for IP-RAW (make IP-header by yourself)
     int sock = -1;
-    if ((sock = socket (AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) 
+    if ((sock = socket (AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1) 
     {
         fprintf (stderr, "socket() failed with error: %d"
 #if defined _WIN32
@@ -204,6 +280,27 @@ int main ()
 #endif
 			);
         fprintf (stderr, "To create a raw socket, the process needs to be run by Admin/root user.\n\n");
+        return -1;
+    }
+
+    // This socket option IP_HDRINCL says that we are building IPv4 header by ourselves, and
+    // the networking in kernel is in charge only for Ethernet header.
+    //
+    const int flagOne = 1;
+    if (setsockopt (sock, IPPROTO_IP, IP_HDRINCL,
+#if defined _WIN32
+		(const char*)
+#endif
+		&flagOne, // The above casting is important for Windows.
+		sizeof (flagOne)) == -1) 
+    {
+        fprintf (stderr, "setsockopt() failed with error: %d"
+#if defined _WIN32
+			, WSAGetLastError()
+#else
+			, errno
+#endif
+			);
         return -1;
     }
 
